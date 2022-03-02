a3596381dcd76c9c6bea9ea226d3484aafba7914
loveandsheep <sheep@backspace.tokyo>
2022-03-02 09:56:06 +0900

    trade

--- createNewBlog.ps1
+++ createNewBlog.ps1
@@ -1,3 +1,5 @@
+Param([switch]$trade)
+
 #今日の日付
 $TODAY = Get-Date -Format "yyMMdd_"
 
@@ -5,14 +7,14 @@ $TODAY = Get-Date -Format "yyMMdd_"
 $BLOGTIME = Get-Date -UFormat "%Y-%m-%dT%T%Z"
 
 #保存先のディレクトリ名
-$DIRNAME = "content/blog/" + $TODAY + $Args[0]
+$DIRNAME = "content/blog/" + $TODAY + "trade"
 
 #テンプレートファイルを
 mkdir $DIRNAME
 
 #テンプレートを置換で書き換える
 $MDFILE = $DIRNAME + "/index.md"
-Copy-Item "content/template/index.md" $MDFILE
+Copy-Item "content/template/trade.md" $MDFILE
 
 $fileContent = $(Get-Content -Encoding UTF8 $MDFILE) -replace "date:", $("date: `"" + $BLOGTIME + "`"")
 $fileContent | Out-File $($MDFILE) -Encoding utf8
